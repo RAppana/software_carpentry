58ce66fad1245f7d5b482864308b729292488d66
ravi <ravi.appana@wsu.edu>
2018-03-01 11:14:42 -0800

    added a comment

--- test1.py
+++ test1.py
@@ -6,5 +6,7 @@ Created on Thu Mar  1 10:30:14 2018
 @author: raviappana
 """
 import pandas as pd
+# working with oceania data
+
 oceania= pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_oceania.csv')
 # print('my first Git experience')
