1804b9afcec57e6383d7247d422d1f76765e0815
Ravi Appana <33162513+RAppana@users.noreply.github.com>
2018-03-01 11:10:52 -0800

    added index_column

--- test1.py
+++ test1.py
@@ -6,5 +6,5 @@ Created on Thu Mar  1 10:30:14 2018
 @author: raviappana
 """
 import pandas as pd
-oceania= pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_oceania.csv')
+oceania= pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_oceania.csv',index_col='country')
 # print('my first Git experience')
