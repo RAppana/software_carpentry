733aade5f31f69ff148373716c6ad9a0d975bf1e
ravi <ravi.appana@wsu.edu>
2018-03-01 10:50:50 -0800

    europe data

---
+++ test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 10:30:14 2018
+
+@author: raviappana
+"""
+import pandas as pd
+data = pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_europe.csv')
+# print('my first Git experience')
