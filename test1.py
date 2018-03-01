305e77c6ba4753d2654434fd32fd3b45518f84bc
ravi <ravi.appana@wsu.edu>
2018-03-01 11:05:34 -0800

    used oceania data instead

--- test1.py
+++ test1.py
@@ -6,5 +6,5 @@ Created on Thu Mar  1 10:30:14 2018
 @author: raviappana
 """
 import pandas as pd
-data = pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_europe.csv')
+oceania= pd.read_csv('/Users/raviappana/Desktop/my_project/data/gapminder_gdp_oceania.csv')
 # print('my first Git experience')
